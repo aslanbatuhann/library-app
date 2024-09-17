f1f28151e9848e25f16c2b2741f4e8f697f5697a
Batuhan Aslan <batuhanaslan9601@gmail.com>
2024-09-17 19:43:41 +0300

    Entity Setup

---
+++ 02-backend/spring-boot-library/src/main/java/com/batuhanaslan/spring_boot_library/entity/Book.java
@@ -0,0 +1,38 @@
+package com.batuhanaslan.spring_boot_library.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "book")
+@Setter
+@Getter
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "author")
+    private String author;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "copies")
+    private int copies;
+
+    @Column(name = "copies_available")
+    private int copiesAvailable;
+
+    @Column(name = "category")
+    private String category;
+
+    @Column(name = "img")
+    private String img;
+
+}
